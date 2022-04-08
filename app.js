9642d77ed663ffe63424d3410292ca07c7349d0f
Benny623 <bmilner88@gmail.com>
2022-04-08 15:31:38 -0600

    added functions to app.js

--- app.js
+++ app.js
@@ -1,2 +1,63 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
+
+function init() {
+    function options() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                choices: ['view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+            },
+        ]).then(input => {
+            switch(input.action) {
+                case 'view all roles':
+                    viewRoles();
+                    break;
+                case 'view all employees':
+                    viewEmployees();
+                    break;
+                case 'add a department':
+                    addDepartment();
+                    break;
+                case 'add a role':
+                    addRole();
+                    break;
+                case 'add an employee':
+                    addEmployee();
+                    break;
+                case 'update an employee role':
+                    updateRole();
+                    break;
+            };
+        });
+    };
+
+    function viewRoles() {
+
+    };
+
+    function viewEmployees() {
+
+    };
+
+    function addDepartment() {
+
+    };
+
+    function addRole() {
+
+    };
+
+    function addEmployee() {
+
+    };
+
+    function updateRole() {
+
+    };
+    
+    options();
+};
+
+init();
