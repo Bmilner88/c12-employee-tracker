dbeb90bb566df8e7eee64f3e2723790716243939
Benny623 <bmilner88@gmail.com>
2022-04-11 00:44:49 -0600

    added restart()

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function init() {
             {
                 type: 'list',
                 name: 'action',
+                message: 'What would you like to do?',
                 choices: ['view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
             },
         ]).then(input => {
@@ -33,14 +34,32 @@ function init() {
         });
     };
 
+    function restart() {
+        inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'confirm_restart',
+                message: 'Would you like to do something else?',
+                default: false
+            }
+        ])
+        .then(confirm => {
+            if(confirm.confirm_restart) {
+                options();
+            }
+        });
+    };
+
     function viewRoles() {
         const sql = `SELECT * FROM roles`;
+
         db.query(sql, (err, rows) => {
             if(err) {
                 console.log(err.message);
                 return;
             }
             console.table(rows);
+            restart();
         });
     };
 
